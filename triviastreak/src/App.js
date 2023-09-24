6823959b4782211e3d565390d25631ec8012a23b
Kyle Labelle <klabe099@uottawa.ca>
2023-09-24 02:12:57 -0400

    completed task 2

--- triviastreak/src/App.js
+++ triviastreak/src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [question, setQuestion] = useState('');
+  const [correctAnswer, setCorrectAnswer] = useState('');
+
+  const fetchQuestion = async () => {
+    try {
+      const apiUrl = 'https://opentdb.com/api.php';
+      const amount = 1;
+      const difficulty = 'easy';
+      const type = 'boolean';
+      const category = 9; // General Knowledge
+
+      const response = await fetch(`${apiUrl}?amount=${amount}&difficulty=${difficulty}&type=${type}&category=${category}`);
+
+      if (response.status === 200) {
+        const data = await response.json();
+        if (data.results.length > 0) {
+          const questionData = data.results[0];
+          setQuestion(questionData.question);
+          setCorrectAnswer(questionData.correct_answer);
+        } else {
+          console.log('No results found.');
+        }
+      } else {
+        console.error('Error fetching question:', response.statusText);
+      }
+    } catch (error) {
+      console.error('An error occurred:', error);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Trivia Streak</h1>
+      <button onClick={fetchQuestion}>Fetch Question</button>
+      {question && (
+        <div>
+          <p>Question: {question}</p>
+          <p>Correct Answer: {correctAnswer}</p>
+          <button>True</button>
+          <button>False</button>
+        </div>
+      )}
     </div>
   );
 }
